6cf15e759345faf54d23087bd76d43c4afabac79
gibbsa <39273120+gibbsa@users.noreply.github.com>
2018-12-11 11:57:58 -0500

    Update test.ps1
    update script

--- artifacts/scripts/test.ps1
+++ artifacts/scripts/test.ps1
@@ -1,1 +1,7 @@
-$json 
+# Write your powershell commands here.
+
+$json = get-content -path $env:BUILD_SourcesDirectory\artifacts\test.txt | out-string | convertfrom-json
+
+Write-Host "##vso[task.setvariable variable=adminUsername;isOutput=true;]$json.parameters.adminUsername.value"
+
+# Use the environment variables input below to pass secret variables to this script.
